31ff6da5aca30bdc4dd95526c9ed069390f4f83d
Merve Sert <merveyildizsert@gmail.com>
2024-10-31 14:28:16 +0300

    email.js downloaded and email section completed

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,10 +1,33 @@
+"use client"
 import React from "react";
 import GithubIcon from "../../public/images/github-icon.svg";
 import LinkedinIcon from "../../public/images/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+import emailjs from "emailjs-com";
 
 const EmailSection = () => {
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        "XXX", // email.js server id
+        "XXX", // email.js template id
+        {
+          to_name: "Merve",
+          from_name: e.target.name.value,
+          from_email: e.target.email.value,
+          message: e.target.message.value,
+        },
+        "XXX" // email.js public key
+      )
+      .then((response) => {
+        alert("Your Email has been sent.");
+      })
+      .catch((error) => {
+        alert("Upps! Your email could not be sent.");
+      });
+  };
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
       <div>
@@ -24,7 +47,23 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label
+              className="text-white block text-sm font-medium"
+              htmlFor="name"
+            >
+              Your Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              placeholder="Joe Doe"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+            />
+          </div>
           <div className="mb-6">
             <label
               className="text-white block text-sm font-medium"
@@ -35,23 +74,9 @@ const EmailSection = () => {
             <input
               type="email"
               id="email"
+              name="email"
               required
               placeholder="doe@gmail.com"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="text-white block mb-2 text-sm font-medium"
-              htmlFor="subject"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              required
-              placeholder="Just say hi!"
               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
             />
           </div>
@@ -69,7 +94,12 @@ const EmailSection = () => {
               placeholder="Let's talk about..."
             />
           </div>
-          <button type="submit" className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 rounded-lg">Send Message</button>
+          <button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 rounded-lg"
+          >
+            Send Message
+          </button>
         </form>
       </div>
     </section>
