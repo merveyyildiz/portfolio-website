108c151628c7bf21138e35af46f8bcf3d28ccaa9
Merve Sert <merveyildizsert@gmail.com>
2024-11-12 14:29:22 +0300

    regex changed

--- app/schemas/contactFormSchema.js
+++ app/schemas/contactFormSchema.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 const contactFormSchema = yup.object().shape({
-    name: yup.string().matches(/^[a-zA-Z_-]+$/, "Invalid characters").required("Required"),
+    name: yup.string().matches(/^[a-zA-Z\s_-]+$/, "Invalid characters").required("Required"),
     email: yup.string().email("Invalid Email").required("Required"),
     message: yup.string().required("Required")
 });
